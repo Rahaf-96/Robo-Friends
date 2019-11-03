fac24493afd24ddbc21319fbbcfddc7a281c383b
Rahaf-96 <rahaf.hawamdeh96@gmail.com>
2019-11-03 02:13:56 +0200

    Updates

---
+++ precache-manifest.c9d01821f923112a3fba21436150e9a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833da2178af3ed86da25e1c1dc6e4490",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "20b6f5009d51a1345a26",
+    "url": "/Robo-Friends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "22459fa05f5df286ef7e",
+    "url": "/Robo-Friends/static/css/main.ceb20cea.chunk.css"
+  },
+  {
+    "revision": "20b6f5009d51a1345a26",
+    "url": "/Robo-Friends/static/js/2.110e328c.chunk.js"
+  },
+  {
+    "revision": "22459fa05f5df286ef7e",
+    "url": "/Robo-Friends/static/js/main.08833eb8.chunk.js"
+  },
+  {
+    "revision": "ee750d1916800aff3a45",
+    "url": "/Robo-Friends/static/js/runtime-main.84450878.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
